2bb814c9dd8df09f88f14b57d2b7fde679fae323
Ivan <ivan.r.makarov@yandex.ru>
2019-10-31 23:53:20 +0300

    UPD -- Work with backend

--- web-app/styler-frontend/src/Components/App-content.js
+++ web-app/styler-frontend/src/Components/App-content.js
@@ -14,22 +14,15 @@ class AppContent extends Component {
   }
 
   handleChangeFile = (e) => {
-
-    //console.log(e.target.files[0].type);
-    // if (e.target.files[0].type !== "image/jpeg") {
-    //   console.log(e.target.files[0].type);
-    //   return;
-    // }    
     this.file = e.target.files[0];
-    document.querySelector(".preview > img").src = window.URL.createObjectURL(this.file);
+    document.querySelector(".images > img.preview").src = window.URL.createObjectURL(this.file);
   }
 
   handleUploadFile = (e) => {
     var file = this.file;
-
-    // Create a new tus upload
+    
     var upload = new tus.Upload(file, {
-        endpoint: "http://localhost:1080/files/",
+        endpoint: "http://127.0.0.1:1080/",
         retryDelays: [0, 3000, 5000, 10000, 20000],
         metadata: {
             filename: file.name,
@@ -44,10 +37,10 @@ class AppContent extends Component {
         },
         onSuccess: function() {
             console.log("Download %s from %s", upload.file.name, upload.url)
+            document.querySelector(".images > img.ready").src = upload.url;
         }
     })
 
-    // Start the upload
     upload.start()
 
   }
@@ -59,8 +52,9 @@ class AppContent extends Component {
           handleChangeFile={this.handleChangeFile}
           handleUploadFile={this.handleUploadFile}
         />
-        <div className="preview">
-          <img src={logo} />
+        <div className="images">
+          <img className="preview" src={logo} />
+          <img className="ready" src={logo} />
         </div>
       </div>
     );
